ed5c6c34087a4fab5cb2929c46b54df481236e58
liupeidong <liupeidong@kuaishou.com>
2020-05-29 15:58:06 +0800

    remove

--- src-ts/color.ts
+++
@@ -1,5 +0,0 @@
-namespace Color {
-    export const font = '#333333'
-}
-
-export default Color
--- src-ts/dom.ts
+++
@@ -1,6 +0,0 @@
-export function createDom(tag = 'div', className?, id?) : HTMLElement {
-    const o = document.createElement(tag)
-    className && (o.className = className)
-    id && (o.id = id)
-    return o
-}
